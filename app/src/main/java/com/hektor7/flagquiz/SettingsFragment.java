a60988548385f9cee476528d8ab4f67846113959
Héctor Vizcaíno Cortegano <hektor7@gmail.com>
2014-07-28 18:58:30 +0200

    Commit inicial. FlagQuiz funcionando sobre SGSII.

---
+++ app/src/main/java/com/hektor7/flagquiz/SettingsFragment.java
@@ -0,0 +1,16 @@
+// SettingsFragment.java
+// Subclass of PreferenceFragment for managing app settings
+package com.hektor7.flagquiz;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+    // creates preferences GUI from preferences.xml file in res/xml
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences); // load from XML
+    }
+} // end class SettingsFragment
+
